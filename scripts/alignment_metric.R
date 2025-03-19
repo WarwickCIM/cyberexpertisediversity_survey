294720d5a60fa2aca7fd655618c88cefa5a1f82a
Carlos Cámara <carlos.camara@warwick.ac.uk>
2025-03-19 10:26:13 +0000

    Remove four permutations.

--- scripts/alignment_metric.R
+++ scripts/alignment_metric.R
@@ -259,8 +259,6 @@ calc_permuted_index <- function(df, permutations_n = 3) {
 
 scenarios_alignment_permutations_2 <- calc_permuted_index(scenarios_alignment_summary, 2)
 scenarios_alignment_permutations_3 <- calc_permuted_index(scenarios_alignment_summary, 3)
-scenarios_alignment_permutations_4 <- calc_permuted_index(scenarios_alignment_summary, 4)
 
 write.csv(scenarios_alignment_permutations_2, "data/processed/scenarios_alignment_permutations_2.csv")
 write.csv(scenarios_alignment_permutations_3, "data/processed/scenarios_alignment_permutations_3.csv")
-write.csv(scenarios_alignment_permutations_4, "data/processed/scenarios_alignment_permutations_4.csv")
