75cb2d2bdae9b4cf372a00b214b386128fcbe124
Carlos Cámara <carlos.camara@warwick.ac.uk>
2025-03-19 16:32:31 +0000

    Set default parameters per project
    Code blocks will not be rendered by default nor show warnings, unless overriden.

---
+++ scripts/setup.R
@@ -0,0 +1,11 @@
+# This script contains the libraries that will be used across the project.
+
+library(ggplot2)
+library(gridExtra)
+library(ggbeeswarm)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(skimr)
+library(lubridate)
+library(knitr)
